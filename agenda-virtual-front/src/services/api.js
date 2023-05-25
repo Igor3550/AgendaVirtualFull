168a2a21fba708cff2e97c5d0b6f8e14cc1e54b1
Igor3550 <igormeloigormelo@gmail.com>
2023-05-24 23:25:31 -0300

    Docker

--- agenda-virtual-front/src/services/api.js
+++ agenda-virtual-front/src/services/api.js
@@ -1,64 +1,66 @@
-import axios from "axios";
+import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_BASE_URL // 'http://localhost/api'
+// const baseURL = process.env.REACT_APP_BASE_URL // 'http://localhost/api'
+
+const api = axios.create({baseURL:'http://localhost/api'})
 
 export async function getScheduleList() {
-  const result = await axios.get(`${baseURL}/schedule`);
+  const result = await api.get(`schedules`);
   return result.data;
 }
 
 export async function getDayHours(date) {
-  const res = await axios.get(`${baseURL}/date/hours/${date}`);
+  const res = await api.get(`/date/hours/${date}`);
   return res.data;
 }
 
 export async function getServices() {
-  const res = await axios.get(`${baseURL}/services`);
+  const res = await api.get(`/services`);
   return res.data;
 }
 
 export async function getWaiting() {
-  const res = await axios.get(`${baseURL}/waiting`);
+  const res = await api.get(`/waiting`);
   return res.data;
 }
 
 export async function getHistory(name = null) {
-  const res = await axios.get(`${baseURL}/history?name=${name}`);
+  const res = await api.get(`/history?name=${name}`);
   return res.data;
 }
 
 export async function createSchedule(body) {
-  const res = await axios.post(`${baseURL}/schedule`, body);
+  const res = await api.post(`/schedule`, body);
 
   return res;
 }
 
 export async function createWaiting(body) {
-  const res = await axios.post(`${baseURL}/waiting`, body);
+  const res = await api.post(`/waiting`, body);
 
   return res;
 }
 
 export async function updateSchedule(body, id) {
-  const res = await axios.put(`${baseURL}/schedule/${id}`, body);
+  const res = await api.put(`/schedule/${id}`, body);
 
   return res;
 }
 
 export async function deleteSchedule(id) {
-  const res = await axios.delete(`${baseURL}/schedule/${id}`);
+  const res = await api.delete(`/schedule/${id}`);
 
   return res;
 }
 
 export async function deleteWaiting(id) {
-  const res = await axios.delete(`${baseURL}/waiting/${id}`);
+  const res = await api.delete(`/waiting/${id}`);
 
   return res;
 }
 
 export async function finishSchedule(id) {
-  const res = await axios.put(`${baseURL}/schedule/finish/${id}`);
+  const res = await api.put(`/schedule/finish/${id}`);
 
   return res;
 }
